3d5efcdab24bea65ff3ee5937ff73830b48da47b
Richard Huang <flyerhzm@gmail.com>
2010-07-03 22:29:17 +0800

    use inheritance user for factories

--- templates/authlogic/spec/factories/users.rb
+++ templates/authlogic/spec/factories/users.rb
@@ -1,4 +1,11 @@
-Factory.define :user_flyerhzm, :class => User do |u|
+Factory.define :user do |u|
+  u.login "user"
+  u.password "user"
+  u.password_confirmation "user"
+  u.email "user@gmail.com"
+end
+
+Factory.define :user_flyerhzm, :parent => :user do |u|
   u.login "flyerhzm"
   u.password "flyerhzm"
   u.password_confirmation "flyerhzm"
